2c49821e42a18ee439a1e9db048a34205c6f0442
pokujopo <pokujopo@gmail.com>
2026-09-06 02:18:00 -0700

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,7 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Gate;
-use Illuminate\Support\Facades\URL;
+//use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,9 +21,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
         {    
-            if (app()->environment('production') || config('app.url') !== 'http://localhost') {
+           /* if (app()->environment('production') || config('app.url') !== 'http://localhost') {
             URL::forceScheme('https');
-                }
+                }*/
             Gate::define('admin', function ($user) {
                 return $user->role === 'admin';
             });
