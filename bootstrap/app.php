b1d8231694c21374e409f9d280ff37edafec3caa
pokujopo <pokujopo@gmail.com>
2026-09-06 01:15:46 -0700

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -18,6 +18,10 @@ return Application::configure(basePath: dirname(__DIR__))
     )
 
     ->withMiddleware(function (Middleware $middleware): void {
+            $middleware->use([
+                \Illuminate\Http\Middleware\HandleCors::class,
+            ]);
+
             $middleware->alias([
             'role' => RoleMiddleware::class,
         ]);
